51b0d87bb57fff7e85578016829fb003be51cf06
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-24 16:20:05 +0530

    Added user input data and session

--- grading_system.py
+++ grading_system.py
@@ -1,10 +1,28 @@
-from flask import Flask ,render_template,request,redirect,url_for
+from flask import Flask ,render_template,request,redirect,url_for,session,Response
 
 app = Flask(__name__)
+app.secret_key = "myapp"
 
+#Taking input from User
+def data():
+    name = str(input("Enter your name: "))
+    math = float(input("Enter marks of Math: "))
+    science = float(input("Enter marks of Science: "))
+    english = float(input("Enter marks of English: "))
+
+
+#home route
 @app.route("/")
 def home():
-    pass
+    username = request.form.get("username")
+    password = request.form.get("password")
 
+    if username == "admin" and password == "12345":
+        session['student'] = username
+
+    else:
+        return Response("Invalid credentials",mimetype="text/plain")
+
+#Run flask server
 if __name__ == "__main__":
     app.run(debug=True)
