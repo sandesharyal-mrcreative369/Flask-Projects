07dbaabaf7d52b101b27c809fb817fe1ce679523
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-24 18:42:38 +0530

    Added comments on example.py

--- template_inheritance/example.py
+++ template_inheritance/example.py
@@ -1,21 +1,27 @@
+#Imported Libraries
 from flask import (
 Flask,render_template
 )
 
+#Create and Flask application
 app = Flask(__name__)
 
+#Home route
 @app.route("/")
 def home():
     return render_template("home.html")
 
+#about route
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+#welcome route
 @app.route("/welcome")
 def contact():
     return render_template("welcome.html")
 
 
+#Run Flask Server
 if __name__ == "__main__":
     app.run(debug=True)
