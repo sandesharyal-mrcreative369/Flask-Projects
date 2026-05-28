83c79522eb508c6c6b5922ce57881a38c2af390b
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-28 10:53:10 +0530

    Adding session and flash messages in our code

--- onetime_messages/main.py
+++ onetime_messages/main.py
@@ -1,10 +1,26 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import (Flask,render_template,
+                   request,redirect,url_for,session,
+                   flash)
 
 app = Flask(__name__)
+session.secret_key = 'mySecretKey'
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
-    pass
+    if request.method == 'POST':
+        name = request.form.get('name')
+        message = request.form.get('message')
+
+        if not name:
+            flash("Name not provided")
+            return render_template('form.html')
+
+        flash(f"Thank you {name} for your feedback messages")
+        return redirect(url_for('thankyou'))
+
+    return render_template('form.html')
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
