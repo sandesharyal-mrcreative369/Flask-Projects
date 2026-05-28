21b6d01fc77ffefb92d129a3311aebc638d70daa
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-28 14:48:00 +0530

    Added Registration form class

---
+++ advanced_forms/registration_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,EmailField
+from wtforms.validators import DataRequired,Email,Length
+
+class RegistrationForm(FlaskForm):
+    name = StringField("Full name",validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired(),Length(min=8,max=20)])
+    email = EmailField("Email",validators=[DataRequired(),Email()])
+    submit = SubmitField("Register")
